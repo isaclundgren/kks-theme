ee31d2708cf96c6efaf5489403916cb3a27c2575
isaclundgren <isac.lundgren@gmail.com>
2020-05-19 11:47:51 +0200

    custom logo in hero image

--- global-templates/customhero.php
+++ global-templates/customhero.php
@@ -16,8 +16,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 <section id="hero" class="img-fluid d-flex align-items-center" style="background-image:linear-gradient(rgba(102,102,102,0.6), rgba(102,102,102,0.6)) ,url('<? echo the_field('background_image')['url'] ?>');">
     <div class="container">
-        <h1 class="text-center"><?the_field('hero_title')?></h1>
-        <h2 class="text-center"><?the_field('hero_subtitle')?></h2>
+        <div class="customlogo d-flex justify-content-center align-items-center">
+            <img src="<? echo the_field('hero_logo')?>" alt="Hero Logo">
+        </div>
+        <h1 class="text-center"><?//the_field('hero_title')?></h1>
+        <h2 class="text-center"><?//the_field('hero_subtitle')?></h2>
         <div class="btn d-flex justify-content-center">
             <a href="#" class="btn btn-primary text-center mb-2 herobtn">Click me</a>
         </div>
