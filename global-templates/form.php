14f652359cfdb1e59a9e4c291a48d4d38d3eb07f
isaclundgren <isac.lundgren@gmail.com>
2020-05-22 11:40:57 +0200

    Some styling for contactform

--- global-templates/form.php
+++ global-templates/form.php
@@ -14,11 +14,13 @@ defined( 'ABSPATH' ) || exit;
 
 <div class="wrapper" id="wrapper-contact">
     <div class="container">
+        <div class="center d-flex justify-content-center mb-3">
+        <i class="fa fa-envelope fa-3x"></i>
+        </div>
         <h3 class="text-center"><?_e('Contact us', 'kks-theme')?></h3>
         <div class="row">
             <?get_template_part('loop-templates/content', 'form');?> 
         </div> 
-        <hr>
     </div>
 </div>
 
